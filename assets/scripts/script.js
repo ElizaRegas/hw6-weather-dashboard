f5a16862e9f7f86a9d43f7e3486e72f2a58caab7
Eliza Regas <elizaregas@gmail.com>
2020-06-14 16:27:18 -0400

    added state to search parameters

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  var timeOfDay = moment().format("h:mm:ss a");
 
   $("#inputForm").on("submit", function (event) {
     event.preventDefault();
     var cityEl = $("#city");
+    var stateEl = $("#state");
     $.getJSON(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
-        cityEl.val() +
+        cityEl.val() + "," + stateEl.val() + "," +
         "&units=imperial&appid=fc53c4afba46f05e9baa04eb35435488",
       function (data) {
         console.log(data);
@@ -31,13 +31,18 @@ $(document).ready(function () {
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
+
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
