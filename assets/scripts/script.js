62bc5de87f359a0368726b9acef5b6889aac9191
Eliza Regas <elizaregas@gmail.com>
2020-06-15 15:31:28 -0400

    bootstrap divs set up in html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     event.preventDefault();
     var cityEl = $("#city");
     var stateEl = $("#state");
-    $.getJSON(
+    $.ajax(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         cityEl.val() +
         "," +
