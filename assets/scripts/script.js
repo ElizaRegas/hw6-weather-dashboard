4601de6436bec3ea4f74e8b944630a92dc506875
Eliza Regas <elizaregas@gmail.com>
2020-06-15 20:52:23 -0400

    forecast dates displaying correctly

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -28,6 +28,27 @@ $(document).ready(function () {
       $("#temp").text(temp);
       $("#humidity").text(humidity);
 
+      for (var i = 1; i <= 5; i++) {
+        console.log(response);
+        var forecastTempMinEl = response.main.temp_min;
+        var forecastTempMaxEl = response.main.temp_max;
+        var forecastDate = moment().add(i, 'days').format("MMMM Do");
+        // var forecastIcon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        // var iconPic = $("#forecast" + i).attr("src", forecastIcon);
+        // console.log(iconPic);
+        // var displayTempMin = $("<div id='TempMin'></div>");
+        // var displayTempMax = $("<div id='TempMax'></div>");
+
+        // $(displayTempMin).append("Low: " + Math.floor(forecastTempMinEl));
+        // $(displayTempMax).append("High: " + Math.floor(forecastTempMaxEl));
+        // $("#forecastTempMax").append("High: " + Math.floor(forecastTempMaxEl));
+        // var newLow = $("<div class='low'></div>");
+        // var newIcon = $("<div class='icon'>" + iconPic + "</div>");
+        var dateDiv = $("<div class='date'>" + forecastDate + "</div>");
+        var newCol = $("<div class='col' id='forecast'" + i + "></div>").append(dateDiv);
+        $("#forecastDiv").append(newCol);
+      }
+
       var uvqueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=fc53c4afba46f05e9baa04eb35435488&lat=" + lat + "&lon=" + lon;
       
       $.ajax({
@@ -47,59 +68,17 @@ $(document).ready(function () {
         $("#uvIndex").append("UV Index: ").append(uvButtonEl);
       })
     });
+  })
+});
 
 
-
-    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityEl.val() + "," + "&units=emperial&appid=fc53c4afba46f05e9baa04eb35435488";
-    $.ajax({
-      url: forecastQueryURL,
-      method: "GET",
-    }).then(function(response) {
-      console.log(response);
-    })
-    //     for (var i = 1; i <= 5; i++) {
-    //       var forecastTempMinEl = data.list[i].main.temp_min;
-    //       var forecastTempMaxEl = data.list[i].main.temp_max;
-    //       var forecastDate = moment().add(i, 'days').format("MMMM Do");
-    //       var forecastIcon = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png";
-    //       var iconPic = $(".icon").attr("src", forecastIcon);
-    //       var displayTempMin = $("<div id='TempMin'></div>");
-    //       var displayTempMax = $("<div id='TempMax'></div>");
-
-    //       $(displayTempMin).append("Low: " + Math.floor(forecastTempMinEl));
-    //       $(displayTempMax).append("High: " + Math.floor(forecastTempMaxEl));
-    //       // $("#forecastTempMax").append("High: " + Math.floor(forecastTempMaxEl));
-    //       var newLow = $("<div class='low'></div>");
-    //       var newIcon = $("<div class='icon'>" + iconPic + "</div>");
-    //       var dateDiv = $("<div class='date'>" + forecastDate + "</div>");
-    //       var newCol = $("<div class='col'></div>").append(dateDiv, newIcon, newLow);
-    //       $(".row").append(newCol);
-    //     }
-    //   }
-    // );
-  });
-
-  // var displayHour = hour > 12 ? hour - 12 : hour;
-  // var hourColumn = $("<div class='col-sm-2 timeCol'></div>").text(
-  //   displayHour + ":00"
-  // );
-  // scheduleColumn = $(
-  //   `<div class='col-sm-8 schedule' id='${
-  //     hour + "a"
-  //   }'><input class="inputText"></div>`
-  // );
-  // var saveColumn = $(
-  //   `<div class="col-sm-2" id="save"><button type="button" class="btn btn-info" id='${
-  //     hour + "b"
-  //   }'><i class="fas fa-save"></i></button></div>`
-  // );
 
 //   function emptyInfo() {
 //     $("#icon").empty();
 //     $("#weather").empty();
 //     $("#temp").empty();
 //   }
-});
+// });
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
