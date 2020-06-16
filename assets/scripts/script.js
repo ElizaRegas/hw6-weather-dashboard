7bd0301a5bdc8460304cc7cab61111258f4a7731
Eliza Regas <elizaregas@gmail.com>
2020-06-16 17:29:06 -0400

    forecast array works

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  var lastSearch = window.localStorage.getItem('city');
+  console.log(lastSearch);
+  $('#city').val(lastSearch);
   $("#inputForm").on("submit", function (event) {
     event.preventDefault();
     $("#uvIndex").empty();
@@ -8,15 +11,15 @@ $(document).ready(function () {
       cityEl.val() +
       "," +
       "&units=imperial&appid=fc53c4afba46f05e9baa04eb35435488";
-
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
       console.log(response);
       var cityDisplayEl = response.name;
-      var dateDisplayEl = moment().format('MMMM Do YYYY');
-      var icon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+      var dateDisplayEl = moment().format("MMMM Do YYYY");
+      var icon =
+        "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
       var temp = "Temperature: " + Math.floor(response.main.temp) + " °F";
       var humidity = "Humidity: " + response.main.humidity + "%";
       var wind = "Wind Speed: " + response.wind.speed.toFixed(1) + " MPH";
@@ -28,37 +31,21 @@ $(document).ready(function () {
       $("#temp").text(temp);
       $("#humidity").text(humidity);
 
-      for (var i = 1; i <= 5; i++) {
-        console.log(response);
-        var forecastTempMinEl = response.main.temp_min;
-        var forecastTempMaxEl = response.main.temp_max;
-        var forecastDate = moment().add(i, 'days').format("MMMM Do");
-        var forecastIcon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-        var iconPic = $("#forecast" + i).attr("src", forecastIcon);
-        // console.log(iconPic);
-        // var displayTempMin = $("<div id='TempMin'></div>");
-        // var displayTempMax = $("<div id='TempMax'></div>");
+      var uvqueryURL =
+        "https://api.openweathermap.org/data/2.5/uvi?appid=fc53c4afba46f05e9baa04eb35435488&lat=" +
+        lat +
+        "&lon=" +
+        lon;
 
-        // $(displayTempMin).append("Low: " + Math.floor(forecastTempMinEl));
-        // $(displayTempMax).append("High: " + Math.floor(forecastTempMaxEl));
-        // $("#forecastTempMax").append("High: " + Math.floor(forecastTempMaxEl));
-        var newLow = $("<div class='low'>Low: " + forecastTempMinEl + "</div>");
-        var newHigh = $("<div class='high'>High: " + forecastTempMaxEl + "</div>");
-        var newIcon = $("<div class='icon'>" + iconPic + "</div>");
-        var dateDiv = $("<div class='date'>" + forecastDate + "</div>");
-        var newCol = $("<div class='col' id='forecast'" + i + "></div>").append(dateDiv, newIcon, newLow, newHigh);
-        $("#forecastDiv").append(newCol);
-      }
-
-      var uvqueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=fc53c4afba46f05e9baa04eb35435488&lat=" + lat + "&lon=" + lon;
-      
       $.ajax({
         url: uvqueryURL,
-        method: "GET"
-      }).then(function(response) {
+        method: "GET",
+      }).then(function (response) {
         var uvNumber = response.value;
         console.log(response.value);
-        var uvButtonEl = $("<button id='uvButton'>" + response.value + "</button>");
+        var uvButtonEl = $(
+          "<button id='uvButton'>" + response.value + "</button>"
+        );
         if (uvNumber <= 3) {
           uvButtonEl.css("background-color", "green");
         } else if (uvNumber > 7) {
@@ -67,12 +54,104 @@ $(document).ready(function () {
           uvIndex.css("background-color", "orange");
         }
         $("#uvIndex").append("UV Index: ").append(uvButtonEl);
-      })
+      });
+
+      // for (var i = 1; i <= 5; i++) {
+      //   console.log(response);
+        
+      //   $.ajax({
+      //     url: "https://api.openweathermap.org/data/2.5/weather?q=" +
+      //     cityEl.val() +
+      //     "," +
+      //     "&units=imperial&appid=fc53c4afba46f05e9baa04eb35435488";
+      //   })
+      //   var forecastTempMinEl = response.main.temp_min;
+      //   var forecastTempMaxEl = response.main.temp_max;
+      //   var forecastIcon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+      //   var newLow = $("<div class='low'>Low: " + forecastTempMinEl + "</div>");
+      //   var newHigh = $("<div class='high'>High: " + forecastTempMaxEl + "</div>");
+      //   var newIcon = $("<img src='" + forecastIcon + "'></div>");
+      //   var dateDiv = $("<div class='date'>" + forecastDate + "</div>");
+      //   var newCol = $("<div class='col' id='forecast'" + i + "></div>").append(dateDiv, newIcon, newLow, newHigh);
+      //   // $("#forecast" + i).attr("src", forecastIcon);
+      //   $("#forecastDiv").append(newCol);
+      // }
+      // var cityId = response.sys.id;
     });
-  })
+    var forecastQuery =
+      "https://api.openweathermap.org/data/2.5/forecast/?q=" +
+      cityEl.val() +
+      "&units=imperial&appid=a5e5240beae7c329a8e79847c343d8d3";
+      console.log(forecastQuery);
+
+    $.ajax({
+      url: forecastQuery,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var forecastArray = response.list;
+      console.log(forecastArray);
+
+      // for (var i = 0; i < forecastArray.length; i++) {
+        // var forecastObj = forecastArray[i];
+        var filteredForecastArray = forecastArray.filter(function(listObj) {
+          var timeStamp = listObj.dt_txt;
+          var timeCheck = timeStamp.includes('12:00:00');
+          console.log(timeCheck);
+          return timeCheck; // returning a boolean
+        });
+        console.log(filteredForecastArray);
+
+
+
+        // var timeStamp = forecastObj.dt_txt;
+        // var forecastDate = moment(timeStamp).format("");
+        // console.log(timeStamp);
+        // console.log(forecastDate);
+        // console.log(forecastDate);
+
+        // if (forecastEntry.includes(""))
+      // }
+      // var forecastTempMinEl = response.main.temp_min;
+      // var forecastTempMaxEl = response.main.temp_max;
+      // var forecastIcon =
+      //   "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+      // var newLow = $("<div class='low'>Low: " + forecastTempMinEl + "</div>");
+      // var newHigh = $(
+      //   "<div class='high'>High: " + forecastTempMaxEl + "</div>"
+      // );
+      // var newIcon = $("<img src='" + forecastIcon + "'></div>");
+      // var dateDiv = $("<div class='date'>" + forecastDate + "</div>");
+      // var newCol = $("<div class='col' id='forecast'" + i + "></div>").append(
+      //   dateDiv,
+      //   newIcon,
+      //   newLow,
+      //   newHigh
+      // );
+      // $("#forecast" + i).attr("src", forecastIcon);
+      // $("#forecastDiv").append(newCol);
+    });
+
+    // var cityToBeStored = function (name, key, value) {
+
+    //   // Get the existing data
+    //   var existing = localStorage.getItem(name);
+    
+    //   // If no existing data, create an array
+    //   // Otherwise, convert the localStorage string to an array
+    //   existing = existing ? JSON.parse(existing) : {};
+    
+    //   // Add new data to localStorage Array
+    //   existing[key] = value;
+    
+    //   // Save back to localStorage
+    //   localStorage.setItem(name, JSON.stringify(existing));
+    
+    // };
+
+    window.localStorage.setItem('city', cityEl.val());
+  });
 });
-
-
 
 //   function emptyInfo() {
 //     $("#icon").empty();
