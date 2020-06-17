97a7498d7bce1e2ef0afb6b08aa8da37adf40ed8
Eliza Regas <elizaregas@gmail.com>
2020-06-16 22:46:47 -0400

    stored cities rendering in buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,39 @@
 $(document).ready(function () {
-  // pulling information from local storage to populate weather info from the last searched city
+  // pulling information from local storage to render weather info from the last searched city
   var storedSearches = window.localStorage.getItem("city");
+  // checking to see if the array is empty
   if (storedSearches === null) {
     storedSearches = "[]";
   }
+  // changing json stored string to an array
   storedSearches = JSON.parse(storedSearches);
+  // rendering the last search info to the page on load
   var lastSearchedCity = storedSearches[0];
-  $('#city').val(lastSearchedCity);
-  window.onload = function(event) {
+  $("#city").val(lastSearchedCity);
+  window.onload = function (event) {
     event.preventDefault();
     weatherInformation();
+    return
   };
+  // rendering buttons with the saved searches
+  for (var i = 0; i < 8; i++) {
+    if (storedSearches[i] === "undefined") {
+      return
+    } else {
+      var newButton = $(
+        "<button class='savedSearches'>" + storedSearches[i] + "</button>"
+      );
+      $("#savedSearch").append(newButton);
+    }
+  }
 
   // on-click function for searches
-  $("#inputForm").on("submit", weatherInformation); 
+  $("#inputForm").on("submit", weatherInformation);
 
+  // function to get weather information
   function weatherInformation() {
     $("#uvIndex").empty();
-
+    // info variables
     var cityEl = $("#city");
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -33,9 +49,7 @@ $(document).ready(function () {
       var cityDisplayEl = response.name;
       var dateDisplayEl = moment().format("MMMM Do YYYY");
       var icon =
-        "http://openweathermap.org/img/w/" +
-        response.weather[0].icon +
-        ".png";
+        "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
       var temp = "Temperature: " + Math.floor(response.main.temp) + " °F";
       var humidity = "Humidity: " + response.main.humidity + "%";
       var wind = "Wind Speed: " + response.wind.speed.toFixed(1) + " MPH";
@@ -71,7 +85,6 @@ $(document).ready(function () {
         $("#uvIndex").append("UV Index: ").append(uvButtonEl);
       });
     });
-    
 
     var forecastQuery =
       "https://api.openweathermap.org/data/2.5/forecast/?q=" +
@@ -87,10 +100,8 @@ $(document).ready(function () {
       var filteredForecastArray = forecastArray.filter(function (listObj) {
         var timeStamp = listObj.dt_txt;
         var timeCheck = timeStamp.includes("12:00:00");
-        console.log(timeCheck);
         return timeCheck; // returning a boolean
       });
-      console.log(filteredForecastArray);
 
       // for loop to populate the forecast info
       for (var i = 0; i < filteredForecastArray.length; i++) {
@@ -107,7 +118,9 @@ $(document).ready(function () {
         var temperature = $(
           "<div class='temperature'>Temperature: " + forecastTemp + "</div>"
         );
-        var newHumidEl = $("<div id='newHumid'>Humidity: <br>" + forecastHumidity + "</div>");
+        var newHumidEl = $(
+          "<div id='newHumid'>Humidity: <br>" + forecastHumidity + "</div>"
+        );
         var newIcon = $("<img src='" + forecastIcon + "'/>");
         var dateDiv = $("<div class='date'>" + forecastDate + "</div>");
         var newCol = $("<div class='col' id='forecast'" + i + "></div>").append(
@@ -129,9 +142,8 @@ $(document).ready(function () {
     currentCityArray = JSON.parse(currentCityArray);
     currentCityArray.unshift(cityEl.val());
     window.localStorage.setItem("city", JSON.stringify(currentCityArray));
-  };
+  }
 });
-
 
 // Done
 // WHEN I search for a city
@@ -145,6 +157,7 @@ $(document).ready(function () {
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
+// Done
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 
