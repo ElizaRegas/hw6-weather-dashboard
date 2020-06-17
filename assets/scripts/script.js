a9197771473e71911d8f074ae4be0cf1fcdc90c2
Eliza Regas <elizaregas@gmail.com>
2020-06-16 22:53:30 -0400

    all functionality finish, except one

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -164,5 +164,6 @@ $(document).ready(function () {
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
+// DONE
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
